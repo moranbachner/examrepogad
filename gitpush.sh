2c9a233b501552976d9698e9542dbd6faaf1a165
[moran bachner] <[moranbachner97@gmail.com]>
2023-07-03 17:07:21 +0300

    jlk

---
+++ gitpush.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+read -p "Enter filename to add ('.' = all): " ADD
+
+git add $ADD
+
+read -p "Enter message for commit: " message
+
+echo "current branch = $(git branch --show-current)"
+
+
+url=$1
+git commit -m $message
+
+if [ $# != 0 ]; then
+  git push origin $(git branch --show-current)
+fi
